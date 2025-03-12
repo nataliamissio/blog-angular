fe53149fcccb810191faf482daf16d7fdc7b4f86
natalia <nataalia.missio@gmail.com>
2025-03-12 14:32:00 -0300

    craiando a base dos componentes

---
+++ src/app/components/manu-bar/manu-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manu-bar',
+  imports: [],
+  templateUrl: './manu-bar.component.html',
+  styleUrl: './manu-bar.component.css'
+})
+export class ManuBarComponent {
+
+}
